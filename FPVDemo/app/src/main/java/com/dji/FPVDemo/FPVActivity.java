617084cf3f3c985e827f35764d71087fcac67fde
Ting_Mac <federalkpl@gmail.com>
2015-10-18 14:09:26 +0800

    Completed the DJI FPV Demo Raw view part, nothing wrong.

--- FPVDemo/app/src/main/java/com/dji/FPVDemo/FPVActivity.java
+++ FPVDemo/app/src/main/java/com/dji/FPVDemo/FPVActivity.java
@@ -1,22 +1,45 @@
 package com.dji.FPVDemo;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-
+import java.util.Timer;
+import java.util.TimerTask;
 import dji.sdk.api.DJIDrone;
 import dji.sdk.api.DJIError;
+import dji.sdk.api.Camera.DJICameraSettingsTypeDef.CameraCaptureMode;
+import dji.sdk.api.Camera.DJICameraSettingsTypeDef.CameraMode;
+import dji.sdk.api.DJIDroneTypeDef.DJIDroneType;
+import dji.sdk.interfaces.DJIExecuteResultCallback;
 import dji.sdk.interfaces.DJIGeneralListener;
+import dji.sdk.interfaces.DJIReceivedVideoDataCallBack;
+import dji.sdk.widget.DjiGLSurfaceView;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Process;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FPVActivity extends DemoBaseActivity {
 
     private static final String TAG = "MyApp";
+    private int DroneCode;
+    private DJIReceivedVideoDataCallBack mReceivedVideoDataCallBack = null;
+    private DjiGLSurfaceView mDjiGLSurfaceView;
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fpv);
+
+        DroneCode = 2;
 
         new Thread(){
             public void run(){
@@ -43,5 +66,57 @@ public class FPVActivity extends DemoBaseActivity {
                 }
             }
         }.start();
+
+        onInitSDK(DroneCode);
+        DJIDrone.connectToDrone();
+
+        mDjiGLSurfaceView = (DjiGLSurfaceView)findViewById(R.id.DjiSurfaceView_02);
+        mDjiGLSurfaceView.start();
+
+        mReceivedVideoDataCallBack = new DJIReceivedVideoDataCallBack(){
+            @Override
+            public void onResult(byte[] videoBuffer, int size){
+                mDjiGLSurfaceView.setDataToDecoder(videoBuffer, size);
+            }
+        };
+        DJIDrone.getDjiCamera().setReceivedVideoDataCallBack(mReceivedVideoDataCallBack);
+    }
+
+    private void onInitSDK(int type){
+        switch(type){
+            case 0: {
+                DJIDrone.initWithType(this.getApplicationContext(), DJIDroneType.DJIDrone_Vision);
+                // The SDK initiation for Phantom 2 Vision or Vision Plus
+                break;
+            }
+            case 1: {
+                DJIDrone.initWithType(this.getApplicationContext(), DJIDroneType.DJIDrone_Inspire1);
+                // The SDK initiation for Inspire 1 or Phantom 3 Professional.
+                break;
+            }
+            case 2: {
+                DJIDrone.initWithType(this.getApplicationContext(), DJIDroneType.DJIDrone_Phantom3_Advanced);
+                // The SDK initiation for Phantom 3 Advanced
+                break;
+            }
+            case 3: {
+                DJIDrone.initWithType(this.getApplicationContext(), DJIDroneType.DJIDrone_M100);
+                // The SDK initiation for Matrice 100.
+                break;
+            }
+            default:{
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (DJIDrone.getDjiCamera() != null) {
+            DJIDrone.getDjiCamera().setReceivedVideoDataCallBack(null);
+        }
+        mDjiGLSurfaceView.destroy();
+        super.onDestroy();
+        Process.killProcess(Process.myPid());
     }
 }
